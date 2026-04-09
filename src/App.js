e8015291029b089e306bd1776fdeac3a9a33ead4
“Lee” <lee@glkinc.com>
2026-04-09 13:11:09 -0400

    fix routing for vercel

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // PAGES
 import Landing from "./pages/Home";
